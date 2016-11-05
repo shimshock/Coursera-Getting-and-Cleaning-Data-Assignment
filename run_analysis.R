741cb051513b5b6c1a820c5a2f486d218e55bb69
shimshock <SShimshock@casey.org>
2016-11-04 21:30:35 -0700

    Final code

--- run_analysis.R
+++ run_analysis.R
@@ -22,4 +22,44 @@ features<- read.table("features.txt")
 features<- features[grep("(.mean.|.std.)",features$V2),]
 ##Remove the meanFrequency values
 features<- features[grep(".meanF.",features$V2, invert=TRUE),]
+featurenames<-features[,2]
+
+##select activit lables
+activities<- read.table("activity_labels.txt")
+activitynames<-activities[,2]
+
+##Select the data
+##load features data
+train<-read.table("train/X_train.txt")[featurenames]
+##load activities data
+trainactivities<- read.table("train/y_train.txt")
+##load subject data
+trainsubjects<- read.table("train/subject_train.txt")
+##combine the data
+traincombined<- cbind(trainsubjects,trainactivities,train)
+
+##load the test data
+test <- read.table("test/X_test.txt")[featurenames]
+##load test activities
+testActivities <- read.table("test/Y_test.txt")
+##load test subjects
+testSubjects <- read.table("test/subject_test.txt")
+##combine the data
+testcombined <- cbind(testSubjects, testActivities, test)
+
+
+##merge the data
+datamerge<-rbind(traincombined,testcombined)
+
+##add the labels
+colnames(datamerge)<-c("subject","activity",as.character(featurenames))
+datamerge$activity <- factor(datamerge$activity, levels = activities[,1], labels = activitynames)
+datamerge$subject <- as.factor(datamerge$subject)
+
+##melt data together
+finaldata <- melt(datamerge, id = c("subject", "activity"))
+finaldata_mean_std <- dcast(finaldata, subject + activity ~ variable, mean)
+
+##tiday data set
+write.table(finaldata_mean_std, "tidy.txt", row.names = FALSE, quote = FALSE)
 
